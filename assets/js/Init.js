7e8d3b048e9d81bb6034453fe89318ca8bca2fa4
Pedro Jesus <pedrotiagojesus@gmail.com>
2023-02-10 23:08:47 +0000

    Show calculator result

--- assets/js/Init.js
+++ assets/js/Init.js
@@ -1,30 +1,7 @@
-import { ImcCalculator } from './Calculator.js';
+import { ImcCalculator } from './ImcCalculator.js';
 
 (function () {
 
     const imcCalculator = new ImcCalculator();
-
-    var button = document.querySelector('form button[type="submit"]');
-
-    button.addEventListener("click", async function(event) {
-
-        event.preventDefault();
-
-        const form = document.getElementById('imc-calculator-form');
-        const formData = new FormData(form);
-
-        const Calculator = new CalculatorClass();
-        
-        const height = formData.get('imc-calculator-height');
-        const weight = formData.get('imc-calculator-weight');
-      
-        Calculator.height = height;
-        Calculator.weight = weight;
-
-        const imcValue = await Calculator.calcImc();
-        
-
-
-    });
 
 }());
